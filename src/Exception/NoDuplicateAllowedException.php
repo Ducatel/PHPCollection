c4104c6def4c3e06175a7d7d8ecd3f64b800d7f2
David Ducatel <dducatel@trace.fr>
2016-08-17 21:15:11 +0200

    Add base of a new collection, the set

---
+++ src/Exception/NoDuplicateAllowedException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ducatel\PHPCollection\Exception;
+
+/**
+ * This class define an exception.
+ * This exception will be throw when user try to add a duplicate value in the collection
+ * @author D.Ducatel
+ */
+class NoDuplicateAllowedException extends \Exception
+{
+}
---
+++ src/Set.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Ducatel\PHPCollection;
+
+use Ducatel\PHPCollection\Base\AbstractCollection;
+use Ducatel\PHPCollection\Exception\NoDuplicateAllowedException;
+
+/**
+ * This class represent a array which cannot contains duplicate
+ * Characteristics:
+ *
+ *     - Values: anything, duplicates not allowed
+ *     - Ordering: same as input unless when explicitly sorted
+ *
+ * @package Ducatel\PHPCollection
+ * @author  D.Ducatel
+ */
+class Set extends AbstractCollection implements \ArrayAccess
+{
+
+    /**
+     * Whether a offset exists
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     *                      An offset to check for.
+     *                      </p>
+     *
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->data[$offset]);
+    }
+
+    /**
+     * Offset to retrieve
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to retrieve.
+     *                      </p>
+     *
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return $this->data[$offset];
+    }
+
+    /**
+     * Offset to set
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset The offset to assign the value to.
+     * @param mixed $value  The value to set.
+     *
+     * @throws NoDuplicateAllowedException When try to add an object already added
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        if ($this->contains($value)) {
+            throw new NoDuplicateAllowedException("Object already inserted");
+        }
+
+        if (is_null($offset)) {
+            $this->data[] = $value;
+        } else {
+            $this->data[$offset] = $value;
+        }
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to unset.
+     *                      </p>
+     *
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
+    }
+}
